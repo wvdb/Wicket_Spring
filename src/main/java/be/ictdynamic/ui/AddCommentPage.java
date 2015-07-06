c36e49d580e40a7e96bfb9d93e6c7fc62aa7aa47
Wim Vandenbrande <wim.vandenbrande@hpcds.com>
2015-07-06 16:09:19 +0200

    eerste form

---
+++ src/main/java/be/ictdynamic/ui/AddCommentPage.java
@@ -0,0 +1,49 @@
+package be.ictdynamic.ui;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.RequiredTextField;
+import org.apache.wicket.model.CompoundPropertyModel;
+
+/**
+ * Created by 50038706 on 05/07/2015.
+ */
+public class AddCommentPage extends WebPage {
+    private String author;
+    private String comment;
+
+    public AddCommentPage() {
+        initGui();
+    }
+
+    private void initGui() {
+
+        Form<AddCommentPage> addCommentForm = new Form<AddCommentPage>("addCommentForm", new CompoundPropertyModel<AddCommentPage>(this));
+        add(addCommentForm);
+
+        Label authorLabel = new Label("authorLabel", "Author:");
+        addCommentForm.add(authorLabel);
+
+        RequiredTextField<String> authorField = new RequiredTextField<String>("author");
+        addCommentForm.add(authorField);
+
+        Label commentLabel = new Label("commentLabel", "Comment:");
+        addCommentForm.add(commentLabel);
+
+        RequiredTextField<String> commentField = new RequiredTextField<String>("comment");
+        addCommentForm.add(commentField);
+
+        Button submitButton = new Button("submitButton") {
+            @Override
+            public void onSubmit() {
+                getSession().info("Comment added successfully");
+                setResponsePage(GuestBook.class);
+            }
+        };
+        addCommentForm.add(submitButton);
+    }
+
+}
+
--- src/main/java/be/ictdynamic/ui/GuestBook.java
+++ src/main/java/be/ictdynamic/ui/GuestBook.java
@@ -15,6 +15,7 @@ import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.PropertyListView;
 import org.apache.wicket.model.CompoundPropertyModel;
@@ -23,83 +24,31 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class GuestBook extends WebPage {
-    /** A global list of all comments from all users across all sessions */
-    private static final List<Comment> commentList = Collections.synchronizedList(new ArrayList<Comment>());
-    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+    /**
+     * A global list of all comments from all users across all sessions
+     */
+//    private static final List<Comment> commentList = Collections.synchronizedList(new ArrayList<Comment>());
+//    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     /**
      * Constructor that is invoked when page is invoked without a session.
      */
     public GuestBook() {
-        LOGGER.info("starting GuestBook() now");
-        // Add comment form
-        add(new CommentForm("commentForm"));
+        initGUI();
+    }
 
-        // Add commentListView of existing comments
-        add(new PropertyListView<Comment>("comments", commentList) {
+    private void initGUI() {
+        add(new TextField<String>("text").setType(String.class));
+        add(new TextField<String>("author").setType(String.class));
+        add(new Link<WebPage>("addLink1") {
             @Override
-            public void populateItem(final ListItem<Comment> listItem) {
-                listItem.add(new Label("date"));
-                listItem.add(new Label("author"));
-                listItem.add(new MultiLineLabel("text"));
+            public void onClick() {
+                setResponsePage(new AddCommentPage());
             }
-        }).setVersioned(false);
-
-        Button submitButton = new Button("formsubmit") {          // (4)
-            @Override
-            public void onSubmit() {
-                System.out.println("OnSubmit, name = ");
-            }
-        };
-
-        add(submitButton);
+        });
     }
 
-    /**
-     * A form that allows a user to add a comment.
-     */
-    public final class CommentForm extends Form<ValueMap> {
-        public CommentForm(final String id) {
-            // Construct form with no validation listener
-            super(id, new CompoundPropertyModel<ValueMap>(new ValueMap()));
-            LOGGER.info("starting CommentForm()");
-
-            // this is just to make the unit test happy
-            setMarkupId("commentForm");
-
-            add(new TextField<String>("text").setType(String.class));
-            add(new TextField<String>("author").setType(String.class));
-        }
-
-        @Override
-        public final void onSubmit() {
-            LOGGER.info("starting onSubmit()");
-            getSession().info("Adding Comment");
-
-            ValueMap values = getModelObject();
-
-            if (StringUtils.isNotBlank((String)values.get("text"))) {
-                error("Caught a spammer!!!");
-                return;
-            }
-
-            // Construct a copy of the edited comment
-            Comment comment = new Comment();
-
-            comment.setDate(new Date());
-            comment.setText((String) values.get("text"));
-            comment.setAuthor((String) values.get("author"));
-
-            // add using index 0
-            commentList.add(0, comment);
-
-            values.put("text", "");
-            values.put("author", "");
-            getSession().info("Comment added successfully");
-        }
-    }
-
-    public static void clear() {
-        commentList.clear();
-    }
+//    public static void clear() {
+//        commentList.clear();
+//    }
 }
